63b160da1c5304123730b0d611a8a6d696d5df1e
Ben Williams and Kat gifford <kat@>
2017-03-23 16:54:27 -0700

    Add immunity to bullets

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,24 @@
 $(document).ready(function(){
   $("form#form").submit(function(event) {
-
-
-    var a = parseInt($("#side-a").val());
-    var b = parseInt($("#side-b").val());
-    var c = parseInt($("#side-c").val());
+    var a = parseFloat($("#side-a").val());
+    var b = parseFloat($("#side-b").val());
+    var c = parseFloat($("#side-c").val());
 
       event.preventDefault();
-    if ((a === b) && (b === c) && (a === c)) {
-      $("#tri-1").show();
-    } else if ((a === b) || (b === c) || (a === c)) {
-      $("#tri-2").show();
-    } else if (((a + b) < c) || ((b + c) < a) || ((a + c) < b)) {
-      $("#noway").show();
+      if ((a)&&(b)&&(c)) {
+        if ((a <= 0) || (b<=0) || (c<=0)) {
+        $(".answers").text("Not a triangle :(");
+      } else if ((a === b) && (b === c)) {
+        $(".answers").text("Equilateral!");
+      } else if (((a + b) <= c) || ((b + c) <=a) || ((a + c) <= b)) {
+        $(".answers").text("Not a triangle :(");
+      } else if ((a === b) || (b === c) || (a === c)) {
+        $(".answers").text("Isoceles!!");
+      } else {
+        $(".answers").text("Scalene!!");
+      }
     } else {
-      $("#tri-3").show();
+      $(".answers").text("Please fill all fields with numbers.");
     }
-  });
+    });
 });
